dd1a04dd811cc5c25391bfe80ea1cfa5c2e5c16c
Chuzume <chuzume@gmail.com>
2024-07-18 16:07:05 +0900

    アイテム使用時だろうがダッシュができる処理がよく試したら完成してなかった

--- src/client/java/com/chuzbows/mixin/client/UsingItemSprint.java
+++ src/client/java/com/chuzbows/mixin/client/UsingItemSprint.java
@@ -21,11 +21,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(net.minecraft.client.network.ClientPlayerEntity.class)
 public abstract class UsingItemSprint extends AbstractClientPlayerEntity {
 
-// これはイマイチわかっていない
+    // これはイマイチわかっていない
     @Unique
     ClientPlayerEntity target = (ClientPlayerEntity) (Object) this;
 
-// @Shadowと付けると継承元のメソッドを呼び出してもエラーが出なくなる。理由はよくわかっていない。
+    // @Shadowと付けると継承元のメソッドを呼び出してもエラーが出なくなる。理由はよくわかっていない。
     @Shadow
     protected abstract boolean isWalking();
 
@@ -35,23 +35,22 @@ public abstract class UsingItemSprint extends AbstractClientPlayerEntity {
     @Shadow
     protected abstract boolean canVehicleSprint(Entity vehicle);
 
-// コレはおそらく各メソッドで宣言せずとも使えるようになる…？
-    @Unique
-    ItemStack itemStack = target.getActiveItem();
 
     public UsingItemSprint(ClientWorld world, GameProfile profile) {
         super(world, profile);
     }
 
-// ダッシュが開始できるかどうかのメソッドの後ろに処理を付け足して、「CanSprintWhileUsing」インターフェースのアイテムならダッシュ開始できるようにした
+    // ダッシュが開始できるかどうかのメソッドの後ろに処理を付け足して、「CanSprintWhileUsing」インターフェースのアイテムならダッシュ開始できるようにした
     @Inject(method = "canStartSprinting", at = @At("TAIL"), cancellable = true)
     private void canStartSprinting(CallbackInfoReturnable<Boolean> cir) {
+        ItemStack itemStack = target.getActiveItem();
         cir.setReturnValue(!this.isSprinting() && this.isWalking() && this.canSprint() && (!this.isUsingItem() || itemStack.getItem() instanceof CanSprintWhileUsing) && !this.hasStatusEffect(StatusEffects.BLINDNESS) && (!this.hasVehicle() || this.canVehicleSprint(this.getVehicle())) && !this.isFallFlying());
     }
 
-// アイテム使用中はダブルタップでダッシュできなくなるって処理を「CanSprintWhileUsing」インターフェースのアイテムなら無視するようにした。もっといいやり方がある気がする。
+    // アイテム使用中はダブルタップでダッシュできなくなるって処理を「CanSprintWhileUsing」インターフェースのアイテムなら無視するようにした。もっといいやり方がある気がする。
     @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z"))
     private boolean cancelWeaponSlowdown(ClientPlayerEntity instance) {
+        ItemStack itemStack = target.getActiveItem();
         if (itemStack.getItem() instanceof CanSprintWhileUsing) return false;
         else return instance.isUsingItem();
     }
