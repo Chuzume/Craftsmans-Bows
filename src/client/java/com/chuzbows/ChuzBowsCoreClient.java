8585e6f726291c16c9f39d03135a04e2e6d98e3a
Chuzume <chuzume@gmail.com>
2024-07-22 12:32:16 +0900

    不要になったダメージタイプとエンティティに関する処理を削除

--- src/client/java/com/chuzbows/ChuzBowsCoreClient.java
+++ src/client/java/com/chuzbows/ChuzBowsCoreClient.java
@@ -1,17 +1,10 @@
 package com.chuzbows;
 
-import com.chuzbows.render.ShotArrowEntityRenderer;
-import com.chuzbows.init.Entity;
 import net.fabricmc.api.ClientModInitializer;
-//import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 public class ChuzBowsCoreClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		CustomModelPredicateProvider.registerModModels();
-
-		//EntityRendererRegistry.register(Entity.SHOTARROW, ShotArrowEntity);
-		EntityRendererRegistry.register(Entity.SHOTARROW, ShotArrowEntityRenderer::new);
 	}
 }
--- src/client/java/com/chuzbows/CustomModelPredicateProvider.java
+++ src/client/java/com/chuzbows/CustomModelPredicateProvider.java
@@ -8,14 +8,14 @@ import static com.chuzbows.init.item.*;
 
 public class CustomModelPredicateProvider {
 
-    public static void registerModModels(){
+    public static void registerModModels() {
         registerNewBow(SHORT_BOW);
-        registerNewBow(LONG_BOW);
+        registerLongBow();
         registerNewBow(SHOT_CROSSBOW);
-        //registerNewBow(GATLING_BOW);
-
+        registerNewBow(GATLING_CROSSBOW);
     }
 
+    // 最大チャージレベルが1の弓を登録
     private static void registerNewBow(Item bow) {
         ModelPredicateProviderRegistry.register(bow, Identifier.of("pull"), (stack, world, entity, seed) -> {
             if (entity == null) {
@@ -24,8 +24,22 @@ public class CustomModelPredicateProvider {
             if (entity.getActiveItem() != stack) {
                 return 0.0f;
             }
-            return (float)(stack.getMaxUseTime(entity) - entity.getItemUseTimeLeft()) / 20.0f;
+            return (float) (stack.getMaxUseTime(entity) - entity.getItemUseTimeLeft()) / 20.0f;
         });
         ModelPredicateProviderRegistry.register(bow, Identifier.of("pulling"), (stack, world, entity, seed) -> entity != null && entity.isUsingItem() && entity.getActiveItem() == stack ? 1.0f : 0.0f);
+    }
+
+    // ロングボウ用の専用登録処理録
+    private static void registerLongBow() {
+        ModelPredicateProviderRegistry.register(com.chuzbows.init.item.LONG_BOW, Identifier.of("pull"), (stack, world, entity, seed) -> {
+            if (entity == null) {
+                return 0.0f;
+            }
+            if (entity.getActiveItem() != stack) {
+                return 0.0f;
+            }
+            return (float) (stack.getMaxUseTime(entity) - entity.getItemUseTimeLeft()) / 40.0f;
+        });
+        ModelPredicateProviderRegistry.register(com.chuzbows.init.item.LONG_BOW, Identifier.of("pulling"), (stack, world, entity, seed) -> entity != null && entity.isUsingItem() && entity.getActiveItem() == stack ? 1.0f : 0.0f);
     }
 }
