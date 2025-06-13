e2107e9b2925c1bcdbd62d8f0e817aec3669e024
Chuzume <chuzume@gmail.com>
2025-06-13 20:53:50 +0900

    倍率をかける形で移動速度を調整するようにし、速度低下無視と速度変更を同じMixinで担当するようにした

--- src/client/java/com/craftsman_bows/mixin/client/CustomUsingMoveSpeedMixin.java
+++ src/client/java/com/craftsman_bows/mixin/client/CustomUsingMoveSpeedMixin.java
@@ -17,7 +17,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 // 右クリックの長押し中だろうがダッシュできるアイテムに関する処理
 
