d0dda9e69ec5b4ae3285154c936b2b4e58c2d83e
Chuzume <chuzume@gmail.com>
2025-06-19 07:04:56 +0900

    ショートボウ系のアイテムにダブルタップダッシュを、Redirect使わずに実装した。

--- src/client/java/com/craftsman_bows/mixin/client/CustomUsingMoveSpeedMixin.java
+++ src/client/java/com/craftsman_bows/mixin/client/CustomUsingMoveSpeedMixin.java
@@ -72,16 +72,15 @@ public abstract class CustomUsingMoveSpeedMixin extends AbstractClientPlayerEnti
     @Unique
     private boolean wasPressingForwardKeyLastTick = false;
 
-    // アイテム使用しつつもダッシュ可能になる
+    // アイテム使用しつつも前進キー2回でダッシュ可能になる
     @Inject(method = "tickMovement", at = @At(value = "TAIL"))
     private void canStartDoubleTapSprint(CallbackInfo ci) {
-        if (ticksLeftToDoubleTapSprint > 0) {
-            System.out.println("ticksLeftToDoubleTapSprint: " + this.ticksLeftToDoubleTapSprint);
-        }
-        boolean isPressingForward = this.canStartUsingSprinting(); // 今回のフレームの状態
+
+        // 今回のフレームの状態
+        boolean isPressingForward = this.canStartUsingSprinting();
 
         // キーが「今押されていて、前は押されていなかった」時だけ処理する
-        if (isPressingForward && !wasPressingForwardKeyLastTick) {
+        if (this.isIgnoreSlowdown() && isPressingForward && !wasPressingForwardKeyLastTick) {
 
             // 1回だけ実行される処理（押した瞬間）
             if (this.ticksLeftToDoubleTapSprint > 0) {
@@ -89,10 +88,11 @@ public abstract class CustomUsingMoveSpeedMixin extends AbstractClientPlayerEnti
             } else {
                 this.ticksLeftToDoubleTapSprint = 7;
             }
+        }
 
-            if (this.input.playerInput.sprint()) {
-                this.setSprinting(true);
-            }
+        // 使用中でもダッシュ可能なアイテムを使ってるなら、キーでダッシュ開始できる
+        if (this.isIgnoreSlowdown() && this.input.playerInput.sprint()) {
+            this.setSprinting(true);
         }
 
         // 状態を記録（次の tick のために）
@@ -118,19 +118,7 @@ public abstract class CustomUsingMoveSpeedMixin extends AbstractClientPlayerEnti
     // CanStartSprintをちょっといじったもの。アイテム使用中でもダッシュできるようにするのに使う。
     @Unique
     private boolean canStartUsingSprinting() {
-        // 移動速度下がらないアイテムを使っている場合、CanStartSprintからisUsingItemのチェックをスキップしたものになる
-        if (this.isIgnoreSlowdown()) {
-            return !this.isSprinting()
-                    && this.input.hasForwardMovement()
-                    && this.canSprint()
-                    && !this.isBlind()
-                    && (!this.hasVehicle() || this.canVehicleSprint(this.getVehicle()))
-                    && (!this.isGliding() || this.isSubmergedInWater())
-                    && (!this.shouldSlowDown() || this.isSubmergedInWater())
-                    && (!this.isTouchingWater() || this.isSubmergedInWater());
-        }
-        // そうでない場合、元のCanStartSprintの条件になる
-        else return !this.isSprinting()
+        return !this.isSprinting()
                 && this.input.hasForwardMovement()
                 && this.canSprint()
                 && !this.isBlind()
